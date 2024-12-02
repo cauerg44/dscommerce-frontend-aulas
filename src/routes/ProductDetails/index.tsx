57b5f691d6faefa6cfe091fc6e1d37e24d5a8bb2
Cauê Garcia <cauerg7@outlook.com>
2024-12-02 16:39:42 -0300

    Creating a product object as Prop

--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -1,8 +1,28 @@
 import './styles.css'
+import productImgUrl from "../../assets/img-github.jpg";
 import ButtonInverse from "../../components/ButtonInverse";
 import ButtonPrimary from "../../components/ButtonPrimary";
 import HeaderClient from "../../components/HeaderClient";
 import ProductDetailsCard from "../../components/ProductDetailsCard";
+import { ProductDTO } from '../../models/product';
+
+const product: ProductDTO = {
+    id: 2,
+    name: "Smart TV",
+    description: "Esta TV é muito bonita",
+    imgUrl: productImgUrl,
+    price: 2500.99,
+    categories: [
+        {
+            id: 2,
+            name: "Eletrônicos"
+        },
+        {
+            id: 3,
+            name: "Computadores"
+        }
+    ]
+}
 
 export default function ProductDetails() {
     return (
@@ -10,7 +30,7 @@ export default function ProductDetails() {
             <HeaderClient />
             <main>
                 <section id="product-details-section" className="dsc-container">
-                    <ProductDetailsCard />
+                    <ProductDetailsCard product={product}/>
                     <div className="dsc-btn-page-container">
                         <ButtonPrimary />
                         <ButtonInverse />
