d4b9d91f50125d02aaa7e84567dc7608af5eb4eb
Cauê Garcia <cauerg7@outlook.com>
2024-12-02 17:02:48 -0300

    Exercicies Props: buttons, catalog cards

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,8 +1,32 @@
 import './styles.css'
+import productImgUrl from '../../assets/img-github.jpg'
 import HeaderClient from '../../components/HeaderClient'
 import SearchBar from '../../components/SearchBar'
 import CatalogCard from '../../components/CatalogCard'
 import ButtonNextPage from '../../components/ButtonNextPage'
+import { ProductDTO } from '../../models/product'
+
+const product: ProductDTO = {
+    id: 2,
+    name: "Smart TV",
+    description: "Esta TV é muito bonita",
+    imgUrl: productImgUrl,
+    price: 2500.99,
+    categories: [
+        {
+            id: 2,
+            name: "Eletrônicos"
+        },
+        {
+            id: 3,
+            name: "Computadores"
+        },
+        {
+            id: 4,
+            name: "Importados"
+        }
+    ]
+}
 
 export default function Catalog() {
     return (
@@ -13,17 +37,17 @@ export default function Catalog() {
                     <SearchBar />
 
                     <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-                        <CatalogCard />
-                        <CatalogCard />
-                        <CatalogCard />
-                        <CatalogCard />
-                        <CatalogCard />
-                        <CatalogCard />
-                        <CatalogCard />
-                        <CatalogCard />
-                        <CatalogCard />
-                        <CatalogCard />
-                        <CatalogCard />
+                        <CatalogCard product={product}/>
+                        <CatalogCard product={product}/>
+                        <CatalogCard product={product}/>
+                        <CatalogCard product={product}/>
+                        <CatalogCard product={product}/>
+                        <CatalogCard product={product}/>
+                        <CatalogCard product={product}/>
+                        <CatalogCard product={product}/>
+                        <CatalogCard product={product}/>
+                        <CatalogCard product={product}/>
+                        <CatalogCard product={product}/>
                     </div>
 
                     <ButtonNextPage />
