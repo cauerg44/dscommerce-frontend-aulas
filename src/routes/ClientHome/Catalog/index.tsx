fbd93c953e10bfb2aa75a9bd5f772450b8e43186
Cauê Garcia <cauerg7@outlook.com>
2024-12-09 13:18:41 -0300

    First Back-End requests

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -4,7 +4,7 @@ import CatalogCard from '../../../components/CatalogCard'
 import ButtonNextPage from '../../../components/ButtonNextPage'
 import * as productService from '../../../services/product-service'
 import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { ProductDTO } from '../../../models/product'
 
 
 export default function Catalog() {
@@ -12,7 +12,7 @@ export default function Catalog() {
     const [products, setProducts] = useState<ProductDTO[]>([])
 
     useEffect(() => {
-        axios.get("http://localhost:8080/products?size=12")
+        productService.findAll()
         .then(response => {
             setProducts(response.data.content)
         })
