ec7cc12498fcef3e6b9593a17a2965d138d14a57
Cauê Garcia <cauerg7@outlook.com>
2025-01-02 12:40:40 -0300

    Fixing subTotal and total problem

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -2,6 +2,7 @@ import './styles.css'
 import { useEffect, useState } from 'react'
 import * as cartService from '../../../services/cart-service';
 import { OrderDTO, OrderItemDTO } from '../../../models/order';
+import { Link } from 'react-router-dom';
 
 
 
@@ -51,9 +52,11 @@ export default function Cart() {
                     <div className="dsc-btn dsc-btn-blue">
                         Finalizar pedido
                     </div>
-                    <div className="dsc-btn dsc-btn-white">
-                        Continuar comprando
-                    </div>
+                    <Link to={"/catalog"}>
+                        <div className="dsc-btn dsc-btn-white">
+                            Continuar comprando
+                        </div>
+                    </Link>
                 </div>
             </section>
         </main>
