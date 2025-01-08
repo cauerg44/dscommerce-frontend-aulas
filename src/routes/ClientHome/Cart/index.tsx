2beb5ab83937b64140924c4f8e8c3572342478f2
Cauê Garcia <cauerg7@outlook.com>
2025-01-08 11:15:27 -0300

    Context Cart Count

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -1,8 +1,9 @@
 import './styles.css'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import * as cartService from '../../../services/cart-service';
 import { OrderDTO } from '../../../models/order';
 import { Link } from 'react-router-dom';
+import { ContextCartCount } from '../../../utils/context-cart';
 
 
 
@@ -10,9 +11,11 @@ export default function Cart() {
 
     const [cart, setCart] = useState<OrderDTO>(cartService.getCart())
 
+    const { setContextCartCount } = useContext(ContextCartCount)
+
     function handleClearClick() {
         cartService.clearCart()
-        setCart(cartService.getCart())
+        updateCart()
     }
 
     function handleIncreaseItem(productId: number) {
@@ -22,7 +25,13 @@ export default function Cart() {
 
     function handleDecreaseItem(productId: number) {
         cartService.decreaseItem(productId)
-        setCart(cartService.getCart())
+        updateCart()
+    }
+
+    function updateCart() {
+        const newCart = cartService.getCart()
+        setCart(newCart)
+        setContextCartCount(newCart.items.length)
     }
 
     return (
