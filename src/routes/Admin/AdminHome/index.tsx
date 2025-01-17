7c6c4d4d9c9bfec58c8b31370cf1698209027c59
Cauê Garcia <cauerg7@outlook.com>
2025-01-17 15:04:56 -0300

    Axios interceptors

--- src/routes/Admin/AdminHome/index.tsx
+++ src/routes/Admin/AdminHome/index.tsx
@@ -2,10 +2,13 @@ import './styles.css'
 import { useEffect, useState } from 'react'
 import { UserDTO } from '../../../models/user'
 import * as userService from '../../../services/user-service'
+import { useNavigate } from 'react-router-dom'
 
 export default function AdminHome() {
 
     const [user, setUser] = useState<UserDTO>()
+
+    const navigate = useNavigate()
 
     useEffect(() => {
         userService.findMe()
@@ -13,15 +16,13 @@ export default function AdminHome() {
                 setUser(response.data)
                 console.log(response.data)
             })
-            .catch(error => {
-                console.log("Error", error)
-            })
+            
     }, [])
 
     return (
         <main>
             <section id="admin-home-section" className="dsc-container">
-                <h2 className="dsc-section-title dsc-mb20">Bem-vindo à área administrativa, {user?.name}!</h2>
+                <h2 className="dsc-section-title dsc-mb20">Bem-vindo à área administrativa {user?.name}</h2>
             </section>
         </main>
     )
--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -4,12 +4,41 @@ import * as authService from '../services/auth-service'
 
 export function requestBackend(config: AxiosRequestConfig) {
 
-    const headers = config.withCredentials 
+    const headers = config.withCredentials
         ? {
             ...config.headers,
             Authorization: "Bearer " + authService.getAccessToken()
         }
         : config.headers
 
-    return axios( {...config, baseURL: BASE_URL, headers })
+    return axios({ ...config, baseURL: BASE_URL, headers })
+
+    // REQUEST INTERCEPTOR
+    axios.interceptors.request.use(
+        function (config) {
+            // DO SOMETHING BEFORE REQUEST IS SENT
+            return config;
+        },
+        function (error) {
+            // DO SOMETHING WITH REQUEST ERROR
+            return Promise.reject(error);
+        }
+    );
+
+    // RESPONSE INTERCEPTOR
+    axios.interceptors.response.use(
+        function (response) {
+            // DO SOMETHING WITH RESPONSE DATA IF STATUS IS 2xx
+            return response;
+        },
+        function (error) {
+            if (error.response.status === 401) {
+                console.log("Deu 401!!!")
+            }
+            if (error.response.status === 403) {
+                console.log("Deu 403!!!")
+            }
+            return Promise.reject(error);
+        }
+    );
 }
