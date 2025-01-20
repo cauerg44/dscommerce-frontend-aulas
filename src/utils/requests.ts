dbd1fa812e065eea411b1c5a1b9d4b95e36f824b
Cauê Garcia <cauerg7@outlook.com>
2025-01-20 15:19:19 -0300

    Decoding token to get payload

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -14,30 +14,31 @@ export function requestBackend(config: AxiosRequestConfig) {
 
     return axios({ ...config, baseURL: BASE_URL, headers })
 
-    axios.interceptors.request.use(
-        function (config) {
-            // DO SOMETHING BEFORE REQUEST IS SENT
-            return config;
-        },
-        function (error) {
-            // DO SOMETHING WITH REQUEST ERROR
-            return Promise.reject(error);
-        }
-    ); 
-
-    axios.interceptors.response.use(
-        function (response) {
-            
-            return response;
-        },
-        function (error) {
-            if (error.response.status === 401) {
-                history.push("/login")
-            }
-            if (error.response.status === 403) {
-                history.push("/catalog")
-            }
-            return Promise.reject(error);
-        }
-    );
 }
+
+axios.interceptors.request.use(
+    function (config) {
+        // DO SOMETHING BEFORE REQUEST IS SENT
+        return config;
+    },
+    function (error) {
+        // DO SOMETHING WITH REQUEST ERROR
+        return Promise.reject(error);
+    }
+);
+
+axios.interceptors.response.use(
+    function (response) {
+
+        return response;
+    },
+    function (error) {
+        if (error.response.status === 401) {
+            history.push("/login")
+        }
+        if (error.response.status === 403) {
+            history.push("/catalog")
+        }
+        return Promise.reject(error);
+    }
+);
