08764ecb5c7ba6dfc9ad9f7a78c1ec409a1fc5d9
Cauê Garcia <cauerg7@outlook.com>
2025-01-25 16:15:07 -0300

    Event on dialog modal

--- src/components/DialogInfo/index.tsx
+++ src/components/DialogInfo/index.tsx
@@ -1,13 +1,20 @@
 import ButtonPrimary from "../ButtonPrimary";
 
+type Props = {
+    message: string,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+    onDialogClose: Function
+}
 
-export default function DialogInfo() {
+export default function DialogInfo({ message, onDialogClose }: Props) {
 
     return (
-        <div className="dsc-dialog-background">
-            <div className="dsc-dialog-box">
-                <h2>Operação com sucesso!</h2>
-                <ButtonPrimary text="Ok"/>
+        <div className="dsc-dialog-background" onClick={() => onDialogClose()}>
+            <div className="dsc-dialog-box" onClick={(e) => e.stopPropagation()}>
+                <h2>{message}</h2>
+                <div className="dsc-dialog-btn-container" onClick={() => onDialogClose()}>
+                    <ButtonPrimary text="Ok" />
+                </div>
             </div>
         </div>
     )
