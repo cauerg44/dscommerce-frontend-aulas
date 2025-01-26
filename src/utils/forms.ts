de6d69271c8295b41c07ad7e4b297875b5b858c5
Cauê Garcia <cauerg7@outlook.com>
2025-01-26 16:37:00 -0300

    Forms update all

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -12,3 +12,13 @@ export function toValues(inputs: any) {
     }
     return data
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function updateAll(inputs: any, newValues: any) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const newInputs: any = {}
+    for (let name in inputs) {
+        newInputs[name] = { ...inputs[name], value: newValues[name] }
+    }
+    return newInputs
+}
