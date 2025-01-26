00dc5b072e2ef76da0af951ba9adc4ce09801b7a
Cauê Garcia <cauerg7@outlook.com>
2025-01-26 16:10:03 -0300

    Starting form product

--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -8,6 +8,8 @@ import SearchBar from '../../../components/SearchBar'
 import ButtonNextPage from '../../../components/ButtonNextPage'
 import DialogInfo from '../../../components/DialogInfo'
 import DialogConfirmation from '../../../components/DialogConfirmation'
+import ButtonInverse from '../../../components/ButtonInverse'
+import { useNavigate } from 'react-router-dom'
 
 type QueryParams = {
     page: number
@@ -15,6 +17,8 @@ type QueryParams = {
 }
 
 export default function ProductListing() {
+
+    const navigate = useNavigate()
 
     const [dialogInfoData, setDialogInfoData] = useState({
         visible: false,
@@ -45,6 +49,10 @@ export default function ProductListing() {
             })
     }, [queryParams])
 
+    function handleNewProductClick() {
+        navigate("/admin/products/create")
+    }
+
     function handleSearch(searchText: string) {
         setProducts([])
         setQueryParam({ ...queryParams, page: 0, name: searchText })
@@ -63,11 +71,11 @@ export default function ProductListing() {
     }
 
     function handleDialogConfirmationAnswer(answer: boolean, productId: number) {
-        if (answer)         {
+        if (answer) {
             productService.deleteById(productId)
                 .then(() => {
                     setProducts([])
-                    setQueryParam({ ...queryParams, page: 0})
+                    setQueryParam({ ...queryParams, page: 0 })
                 })
                 .catch(error => {
                     setDialogInfoData({
@@ -85,7 +93,9 @@ export default function ProductListing() {
                 <h2 className="dsc-section-title dsc-mb20">Cadastro de produtos</h2>
 
                 <div className="dsc-btn-page-container dsc-mb20">
-                    <div className="dsc-btn dsc-btn-white">Novo</div>
+                    <div onClick={handleNewProductClick}>
+                        <ButtonInverse text='Novo' />
+                    </div>
                 </div>
 
                 <SearchBar onSearch={handleSearch} />
@@ -131,9 +141,9 @@ export default function ProductListing() {
             {
                 dialogConfirmationData.visible &&
                 <DialogConfirmation
-                id={dialogConfirmationData.id} 
-                message={dialogConfirmationData.message}
-                onDialogAnswer={handleDialogConfirmationAnswer}
+                    id={dialogConfirmationData.id}
+                    message={dialogConfirmationData.message}
+                    onDialogAnswer={handleDialogConfirmationAnswer}
                 />
             }
         </main>
