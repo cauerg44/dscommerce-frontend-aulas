85e646b44302f88e617f51d549e7b760ade8ad70
Cauê Garcia <cauerg7@outlook.com>
2025-01-26 10:55:07 -0300

    Delete finish

--- src/components/DialogConfirmation/index.tsx
+++ src/components/DialogConfirmation/index.tsx
@@ -2,22 +2,23 @@ import ButtonInverse from "../ButtonInverse";
 import ButtonPrimary from "../ButtonPrimary";
 
 type Props = {
+    id: number,
     message: string,
     // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
     onDialogAnswer: Function
 }
 
-export default function DialogConfirmation({ message, onDialogAnswer }: Props) {
+export default function DialogConfirmation({ id, message, onDialogAnswer }: Props) {
 
     return (
-        <div className="dsc-dialog-background" onClick={() => onDialogAnswer(false)}>
+        <div className="dsc-dialog-background" onClick={() => onDialogAnswer(false, id)}>
             <div className="dsc-dialog-box" onClick={(e) => e.stopPropagation()}>
                 <h2>{message}</h2>
                 <div className="dsc-dialog-btn-container" >
-                    <div onClick={() => onDialogAnswer(false)}>
+                    <div onClick={() => onDialogAnswer(false, id)}>
                         <ButtonPrimary text="Não" />
                     </div>
-                    <div onClick={() => onDialogAnswer(true)}>
+                    <div onClick={() => onDialogAnswer(true, id)}>
                         <ButtonInverse text="Sim" />
                     </div>
                 </div>
--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -23,6 +23,7 @@ export default function ProductListing() {
 
     const [dialogConfirmationData, setDialogConfirmationData] = useState({
         visible: false,
+        id: 0,
         message: "Tem certeza?"
     })
 
@@ -57,12 +58,24 @@ export default function ProductListing() {
         setDialogInfoData({ ...dialogInfoData, visible: false })
     }
 
-    function handleDeleteClick() {
-        setDialogConfirmationData({ ...dialogConfirmationData, visible: true })
+    function handleDeleteClick(productId: number) {
+        setDialogConfirmationData({ ...dialogConfirmationData, id: productId, visible: true })
     }
 
-    function handleDialogConfirmationAnswer(answer: boolean) {
-        console.log("resposta", answer)
+    function handleDialogConfirmationAnswer(answer: boolean, productId: number) {
+        if (answer)         {
+            productService.deleteById(productId)
+                .then(() => {
+                    setProducts([])
+                    setQueryParam({ ...queryParams, page: 0})
+                })
+                .catch(error => {
+                    setDialogInfoData({
+                        visible: true,
+                        message: error.response.data.error
+                    })
+                })
+        }
         setDialogConfirmationData({ ...dialogConfirmationData, visible: false })
     }
 
@@ -97,7 +110,7 @@ export default function ProductListing() {
                                     <td className="dsc-tb768">{product.price.toFixed(2)}</td>
                                     <td className="dsc-txt-left">{product.name}</td>
                                     <td><img className="dsc-product-listing-btn" src={editIcon} alt="Editar" /></td>
-                                    <td><img onClick={handleDeleteClick} className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar" /></td>
+                                    <td><img onClick={() => handleDeleteClick(product.id)} className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar" /></td>
                                 </tr>
                             ))
                         }
@@ -117,7 +130,8 @@ export default function ProductListing() {
 
             {
                 dialogConfirmationData.visible &&
-                <DialogConfirmation 
+                <DialogConfirmation
+                id={dialogConfirmationData.id} 
                 message={dialogConfirmationData.message}
                 onDialogAnswer={handleDialogConfirmationAnswer}
                 />
