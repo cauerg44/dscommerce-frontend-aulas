cfbc094c9997a0a77446c497cd4f20979c095a3e
Cauê Garcia <cauerg7@outlook.com>
2025-01-29 15:16:20 -0300

    Deploye ready

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import './index.css'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import React from 'react'
+
+window.React = React
 
 createRoot(document.getElementById('root') as HTMLElement).render(
     <App />
